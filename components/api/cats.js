1690c137b81fae23b2128a0b9e1f85ab5a771885
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2023-11-14 00:12:39 -0600

    add data from custom API

--- components/api/cats.js
+++ components/api/cats.js
@@ -1,31 +1,7 @@
-import { credentials } from './credentials'
-
-export const getCatImage = async (catId) => {
-  const TOKEN = credentials['cat-api']
-  const URL = `https://api.thecatapi.com/v1/images/search?breed_ids=${catId}&limit=4`
-
-  try {
-    const request = await fetch(URL, {
-      headers: {
-        'x-api-key': TOKEN,
-      },
-    })
-    return await request.json()
-  }
-  catch (error) {
-    return { error: 'Error fetching cat image', err: error }
-  }
-}
-
 export const getCats = async (limit=100, page=1) => {
-  const TOKEN = credentials['cat-api']
-  const URL = `https://api.thecatapi.com/v1/breeds?limit=${limit}&page=${page}`
+  const URL = `http://192.168.1.2:3000/cats?${limit}&page=${page}`
 
-  const request = await fetch(URL, {
-    headers: {
-      'x-api-key': TOKEN,
-    },
-  })
+  const request = await fetch(URL)
 
   return await request.json()
 }
